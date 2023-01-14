8f121eca13785622f060b5a8a56534b004c639b4
ntu002 <ngoctubn2002@gmail.com>
2023-01-14 23:32:48 +0700

    14/1

---
+++ ThucHanh/nurse.cpp
@@ -0,0 +1,27 @@
+// Nurse
+/*
+Đề bài: ​
+
+Một giám đốc của một bệnh viện muốn lập lịch làm việc cho y tá trong N ngày 1…N.​
+Y tá chỉ có thể làm việc liên tục trong x ngày (K1 <= x <= K2), sau đó phải nghỉ 1 ngày.​
+- Y tá chỉ được nghỉ 1 ngày giữa 2 đợt làm việc liên tiếp. Có nghĩa rằng nếu y tá nghỉ 
+ngày hôm nay, ngày mai y tá đó sẽ phải làm việc.​
+
+Yêu cầu:​
+Tính số cách có thể lập lịch làm việc cho các y tá.​
+
+​Example:​
+Input:​
+6 2 3​
+Output:​
+4​
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+}
